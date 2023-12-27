b9f3d682ffccf4d7475f73290b75ce7022ef5312
Fadi <fadi.97.kh@outlook.com>
2023-12-28 00:09:07 +0200

    new feature

--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,50 @@
-const renderer = new Renderer();
+// const renderer = new Renderer();
 
-function searchRecipes() {
+// function searchRecipes() {
+//     const ingredient = $('#searchInput').val();
+//     const dairy = $('#diaryFree').is(":checked");
+//     const gluten = $('#glutenFree').is(":checked");
+//     const vegetarian = $('#unvegetarianFree').is(":checked");
+
+//     const url = `/recipes/${ingredient}?dairyFree=${dairy}&glutenFree=${gluten}&unvegetarianFree=${vegetarian}`;
+
+//     $.getJSON(url).then((recipes) => {
+//         recipes.unvegetarianFree = vegetarian;
+//         renderer.display(recipes);
+
+//         $(".share-email-btn").on("click", function () {
+//             const title = $(this).data("title") 
+//             const videoUrl = $(this).data("link")
+        
+//             const emailSubject = `Check out this recipe! ${title}`;
+//             const emailBody = `You can see the recipe in this video: ${videoUrl}`;
+
+//             const mailtoLink = `mailto:?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`;
+//             window.location.href = mailtoLink;
+//         });
+//     });
+// }
+
+const renderer = new Renderer();
+function searchRecipes(page = 1) {
     const ingredient = $('#searchInput').val();
     const dairy = $('#diaryFree').is(":checked");
     const gluten = $('#glutenFree').is(":checked");
     const vegetarian = $('#unvegetarianFree').is(":checked");
 
-    const url = `/recipes/${ingredient}?dairyFree=${dairy}&glutenFree=${gluten}&unvegetarianFree=${vegetarian}`;
+    const limit = 5;
+    const offset = (page - 1) * limit;
+    const url = `/recipes/${ingredient}?dairyFree=${dairy}&glutenFree=${gluten}&unvegetarianFree=${vegetarian}&limit=${limit}&offset=${offset}`;
 
     $.getJSON(url).then((recipes) => {
         recipes.unvegetarianFree = vegetarian;
         renderer.display(recipes);
+        updatePaginationButtons(Math.ceil(recipes.totalCount / limit));
 
         $(".share-email-btn").on("click", function () {
-            const title = $(this).data("title") 
-            const videoUrl = $(this).data("link")
-        
+            const title = $(this).data("title");
+            const videoUrl = $(this).data("link");
+
             const emailSubject = `Check out this recipe! ${title}`;
             const emailBody = `You can see the recipe in this video: ${videoUrl}`;
 
@@ -25,7 +54,4 @@ function searchRecipes() {
     });
 }
 
-
-
-  
 
